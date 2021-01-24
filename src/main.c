0787f9bd08d31612992e9914e2f2901d438ff69b
Patrik Mada <mada.patrik@gmail.com>
2021-01-24 11:06:45 +0100

    feat: Add simple text file read

--- src/main.c
+++ src/main.c
@@ -2,6 +2,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define CHUNK 1024
+
+/** Read text file from a given path and save it to the memory */
+char* read_text(char* input_path) {
+
+    char *buf = malloc(CHUNK);
+    if (buf == NULL) {
+        fprintf(stderr, "ERROR: while allocating memory");
+        exit(1);
+    }
+
+    size_t nread;
+
+    FILE *file = fopen(input_path, "r");
+
+    if (file == NULL) {
+        fprintf(stderr, "ERROR: while trying to open `%s' for reading\n", input_path);
+        exit(1);
+    }
+
+    while ((nread = fread(buf, 1, CHUNK, file)) > 0) {
+        fwrite(buf, 1, nread, stdout);
+    }
+
+    fclose(file);
+
+    return buf;
+}
+
 void error_on_improper_use() {
     fprintf(stderr, "ERROR: Failed to parse command line arguments. See README.md for command line input troubleshooting.");
     exit(8);
@@ -51,6 +80,8 @@ int main(int argc, char* argv[]) {
              error_on_improper_use();
         }
     }
+    
+    read_text(input_path);
 
     return 0;
 }
